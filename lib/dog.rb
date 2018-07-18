53dd60a04fdd11f406ce51bd150ff453f6d8747b
Luoana Chirita <luoana.livadariu@gmail.com>
2018-07-18 21:05:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,12 @@ class Dog
   
   def initialize(name)
     @name = name 
-    @@all << self
+    @@all << self.name
   end 
   
   def all 
-    
+    @@all.each do |dog| 
+      
   end 
     
 end 
